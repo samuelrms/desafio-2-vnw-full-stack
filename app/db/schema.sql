08e017489454c5ffe3a22d7c45031948110d3cca
Samuel Ramos <samuelaoliveiraramos@gmail.com>
2025-03-22 00:34:28 -0300

    add schema.sql to define LIVROS table structure in the database

---
+++ app/db/schema.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS LIVROS (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    titulo TEXT NOT NULL,
+    categoria TEXT NOT NULL,
+    autor TEXT NOT NULL,
+    imagem_url TEXT NOT NULL
+);
